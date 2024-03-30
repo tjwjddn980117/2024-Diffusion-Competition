40dcf1bd9200a5afd390481a8159b8b8bf71d9ae
tjwjddn980117 <tjwjddn15584@naver.com>
2024-03-30 23:28:36 +0900

    .

--- models/GaussianDiffusion.py
+++ models/GaussianDiffusion.py
@@ -4,12 +4,13 @@ from torch import sqrt
 from torch.special import expm1
 from torch.cuda.amp import autocast
 import tqdm
+import torch.nn.functional as F
 
-from einops import repeat
+from einops import repeat, reduce
 
 from UViT import UViT
-from ..utils.normalize import unnormalize_to_zero_to_one, logsnr_schedule_cosine, logsnr_schedule_shifted, logsnr_schedule_interpolated
-from ..utils.helpers import exists
+from ..utils.normalize import *
+from ..utils.helpers import exists, default
 
 class GaussianDiffusion(nn.Module):
     def __init__(self, model: UViT, *, image_size, channels = 3, pred_objective = 'v',
@@ -75,6 +76,8 @@ class GaussianDiffusion(nn.Module):
         '''
         extract p mean and variance. 
         the p distribution means that denoise the image. 
+        the mean and variance is only affected by time information. 
+        time gose to ( 1.0 -> 0.0 ). 
 
         Arguments:
             x (tensor): [B, C, H, W]. random noise. 
@@ -145,10 +148,13 @@ class GaussianDiffusion(nn.Module):
     @torch.no_grad()
     def p_sample_loop(self, shape):
         '''
-        p_sample_loop doing keep denoising with num_sample_steps.
+        p_sample_loop doing keep denoising with num_sample_steps. 
 
         Arguments:
-            shape (tuple): 
+            shape (tuple): the shape would be [batch_size, channels, image_size, image_size]
+        
+        Reterns:
+            img (tensor): [B, C, H, W]. 
         '''
         batch = shape[0]
 
@@ -168,4 +174,77 @@ class GaussianDiffusion(nn.Module):
     
     @torch.no_grad()
     def sample(self, batch_size = 16):
+        '''
+        sampling the denoising methods. 
+        
+        Arguments:
+            batch_size (int): the number of batch size. 
+        
+        return:
+            img (tensor): [B, C, H, W]. 
+        '''
         return self.p_sample_loop((batch_size, self.channels, self.image_size, self.image_size))
+    
+    # training related functions - noise prediction
+    @autocast(enabled = False)
+    def q_sample(self, x_start, times, noise = None):
+        '''
+        the function for making noise. 
+
+        Arguments:
+            x_start (tensor): [B, C, H, W]. the start x. 
+            times (float): noise time. 
+        
+        Returns:
+            x_noised (tensor): [B, C, H, W]. the tensor with noise. 
+        '''
+        noise = default(noise, lambda: torch.randn_like(x_start))
+
+        log_snr = self.log_snr(times)
+
+        log_snr_padded = right_pad_dims_to(x_start, log_snr)
+        alpha, sigma = sqrt(log_snr_padded.sigmoid()), sqrt((-log_snr_padded).sigmoid())
+        x_noised =  x_start * alpha + noise * sigma
+
+        return x_noised, log_snr
+
+    def p_losses(self, x_start, times, noise = None):
+        noise = default(noise, lambda: torch.randn_like(x_start))
+
+        x, log_snr = self.q_sample(x_start = x_start, times = times, noise = noise)
+        model_out = self.model(x, log_snr)
+
+        if self.pred_objective == 'v':
+            padded_log_snr = right_pad_dims_to(x, log_snr)
+            alpha, sigma = padded_log_snr.sigmoid().sqrt(), (-padded_log_snr).sigmoid().sqrt()
+            target = alpha * noise - sigma * x_start
+
+        elif self.pred_objective == 'eps':
+            target = noise
+
+        loss = F.mse_loss(model_out, target, reduction = 'none')
+
+        loss = reduce(loss, 'b ... -> b', 'mean')
+
+        snr = log_snr.exp()
+
+        maybe_clip_snr = snr.clone()
+        if self.min_snr_loss_weight:
+            maybe_clip_snr.clamp_(max = self.min_snr_gamma)
+
+        if self.pred_objective == 'v':
+            loss_weight = maybe_clip_snr / (snr + 1)
+
+        elif self.pred_objective == 'eps':
+            loss_weight = maybe_clip_snr / snr
+
+        return (loss * loss_weight).mean()
+    
+    def forward(self, img, *args, **kwargs):
+        b, c, h, w, device, img_size, = *img.shape, img.device, self.image_size
+        assert h == img_size and w == img_size, f'height and width of image must be {img_size}'
+
+        img = normalize_to_neg_one_to_one(img)
+        times = torch.zeros((img.shape[0],), device = self.device).float().uniform_(0, 1)
+
+        return self.p_losses(img, times, *args, **kwargs)
