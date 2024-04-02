d44ea68fedd0ddc89ce102f548bd77a4f3ce4178
tjwjddn980117 <tjwjddn15584@naver.com>
2024-04-02 11:24:55 +0900

    .

---
+++ requirement.bat
@@ -0,0 +1,27 @@
+rem you should change the root with your own environment path root.
+rem and you could change the ENV_NAME with your one vitual environment.
+set root=C:\Users\qowor\anaconda3
+set ENV_NAME=Diffusion
+
+if not exist "%root%" (
+    echo check the root please: %root%
+    pause
+    exit
+)
+
+call %root%\Scripts\activate.bat %root%
+
+echo make the virtual environment '%ENV_NAME%'
+call conda create -y -n %ENV_NAME% python=3.10
+
+echo enter the virtual environment.
+call conda activate %ENV_NAME%
+
+echo start downloading environment for %ENV_NAME%.
+call conda install -y pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+call conda install -y conda-forge::packaging conda-forge::tqdm conda-forge::einops conda-forge::accelerate
+call pip install -y ema-pytorch pytorch-fid
+
+call conda deactivate
+
+echo complete. 
