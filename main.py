2b20b780ea61fcca6a68c5c28b12291586427752
tjwjddn980117 <tjwjddn15584@naver.com>
2024-04-06 23:55:18 +0900

    .

---
+++ main.py
@@ -0,0 +1,28 @@
+from models import UViT, GaussianDiffusion
+from train import Trainer
+
+model = UViT(
+    dim = 64,
+    dim_mults = (1, 2, 4, 8),
+)
+
+diffusion = GaussianDiffusion(
+    model,
+    image_size = 128,
+    timesteps = 1000,           # number of steps
+    sampling_timesteps = 250    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
+)
+
+trainer = Trainer(
+    diffusion,
+    'path/to/your/images',
+    train_batch_size = 32,
+    train_lr = 8e-5,
+    train_num_steps = 700000,         # total training steps
+    gradient_accumulate_every = 2,    # gradient accumulation steps
+    ema_decay = 0.995,                # exponential moving average decay
+    amp = True,                       # turn on mixed precision
+    calculate_fid = True              # whether to calculate fid during training
+)
+
+trainer.train()
