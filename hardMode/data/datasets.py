b79d4b8fb681f0dd37797f73d6732ab3be5bc364
tjwjddn980117 <tjwjddn15584@naver.com>
2024-04-10 17:32:04 +0900

    .

---
+++ hardMode/data/datasets.py
@@ -0,0 +1,42 @@
+from torch import nn
+from pathlib import Path
+from functools import partial
+from torchvision import transforms as T
+
+from PIL import Image
+from torch.utils.data import Dataset
+
+from ..utils.functions import convert_image_to_fn, exists
+
+# dataset classes
+class Dataset(Dataset):
+    def __init__(
+        self,
+        folder,
+        image_size,
+        exts = ['jpg', 'jpeg', 'png', 'tiff'],
+        augment_horizontal_flip = False,
+        convert_image_to = None
+    ):
+        super(Dataset).__init__()
+        self.folder = folder
+        self.image_size = image_size
+        self.paths = [p for ext in exts for p in Path(f'{folder}').glob(f'**/*.{ext}')]
+
+        maybe_convert_fn = partial(convert_image_to_fn, convert_image_to) if exists(convert_image_to) else nn.Identity()
+
+        self.transform = T.Compose([
+            T.Lambda(maybe_convert_fn),
+            T.Resize(image_size),
+            T.RandomHorizontalFlip() if augment_horizontal_flip else nn.Identity(),
+            T.CenterCrop(image_size),
+            T.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        img = Image.open(path)
+        return self.transform(img)
